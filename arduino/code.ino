1d28fb37cf3fac7dc8f2ee34a5d8f63733c1e621
TitusMcKay <titusdmckay@gmail.com>
2026-04-26 22:14:05 -0700

    made code gooder

--- arduino/code.ino
+++ arduino/code.ino
@@ -1,18 +1,54 @@
 #main pin in is d2
+const int fobPin = 2
+const int switchPin = 7
+
+unsigned long startOfPulse=0;
+bool timing = false;
+
 void setup () {
-#  setup things
+  pinMode(fobPin, INPUT);
+  pinMode(switchPin, OUTPUT);
 }
 
 void loop() {
-  if pin is high
+  if (digitalRead(fobPin) == HIGH){
     if (!timing) {
-      startofpulse = mills();
-      timing=true
+      startOfPulse = mills();
+      timing=true;
     }
-    if (startofpulse() - mills() > 2000) {
-      digitalWrite(set the pin to high)
+    if (mills()- startOfPulse() > 2000) {
+      digitalWrite(switchPin, HIGH)
     }
   else {
     do nothing
   }
 }
+/*
+this is code gened by a clanker 🤮, so im hand wrighting the code above 😃👍
+const int inPin  = 2;   // input
+const int outPin = 3;   // output
+
+unsigned long start = 0;
+bool timing = false;
+
+void setup() {
+  pinMode(inPin,  INPUT);
+  pinMode(outPin, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(inPin) == HIGH) {
+    if (!timing) {
+      start = millis();
+      timing = true;
+    }
+    if (millis() - start > 2000) {
+      digitalWrite(outPin, HIGH);   // set pin 3 high
+    }
+  } else {
+    timing = false;
+    digitalWrite(outPin, LOW);      // optional: reset output when input is low
+  }
+}
+*/
+
